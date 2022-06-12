cd7ac53c0900e6a2af02105cef12dacb2e13bad8
dongbok Lee <typoon0820@gmail.com>
2022-06-12 17:14:06 +0900

    Update database.py

--- server/database.py
+++ server/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:typoon0820@ssukzip.c2zosq5snmdo.ap-northeast-2.rds.amazonaws.com/ssukzip"
+SQLALCHEMY_DATABASE_URL = ""
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
